075439f1a06b7e0aecb11d1c0963380658b19d9c
Alex <caowenjieko@gmail.com>
2025-11-14 13:03:23 +0800

    refactor: remove extra file

--- oflow/app/(main)/(tabs)/dashboard.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function Dashboard() {
-  return (
-    <View className="flex-1 bg-white">
-      <Text className="text-2xl font-bold">Dashboard</Text>
-    </View>
-  );
-}
